137ea4441189f19f1d6f14879f4e0eca9a6cc892
Shliskenstickin <deathwalker@mail.ru>
2021-05-05 20:43:56 +0300

    add junit4 platform

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,7 @@
 package ru.netology.service;
 
 import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 
 public class CashbackHackServiceTest extends TestCase {
     CashbackHackService cashbackHackService = new CashbackHackService();
@@ -24,6 +25,35 @@ public class CashbackHackServiceTest extends TestCase {
         assertEquals(expected, actual);
     }
     public void testRemainLowerZero() {
+        int expected = 1000;
+        int actual = cashbackHackService.remain(-1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testJunit5Remain0() {
+        int expected = 1000;
+        int actual = cashbackHackService.remain(0);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testJunit5Remain1000() {
+        int expected = 0;
+        int actual = cashbackHackService.remain(1000);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testJunit5Remain1500() {
+        int expected = 500;
+        int actual = cashbackHackService.remain(1500);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testJunit5RemainLowerZero() {
         int expected = 1000;
         int actual = cashbackHackService.remain(-1);
 
